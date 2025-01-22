1635d3219759342216e527f0a4ea4ea9c8f98b60
levisamuel2005 <levi.samuel@kalvium.community>
2025-01-22 15:57:18 +0530

    created a landing page

---
+++ frontend/client/src/pages/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return(
+        // <div className="min-h-screen flex flex-col justify-center items-center text-center bg-purple-300 p-6">
+        <div class="min-h-screen flex flex-col justify-center items-center text-center bg-gradient-to-r from-gray-400 via-gray-600 to-black">
+
+      <div className="max-w-2xl">
+        <h1 className="text-4xl font-bold text-black">FitTrack</h1>
+        <br />
+        <p className="text-lg text-gray-800">
+          Track your fitness journey, every step of the way!
+        </p>
+        <br />
+        <p className="text-gray-800">
+          FitTrack helps you log your workouts, set daily fitness goals, and visualize your progress with powerful analytics. Join our community of fitness enthusiasts today!
+        </p>
+        </div>
+        </div>
+    )
+}
+
+export default LandingPage;
